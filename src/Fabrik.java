a30452aa94269ebfa57817beadf0c186bde04561
Brandon Lewis <brandon.lewis477@comcast.net>
2020-04-06 21:42:45 -0400

    Simplified fabrik algorithm and added documentation and a helper method

--- src/Fabrik.java
+++ src/Fabrik.java
@@ -1,41 +1,60 @@
 
 public class Fabrik {
-	public static void main(String args[]) {
+	/*public static void main(String args[]) {
 		Vertex[] limb = {new Vertex(0,0,0), new Vertex(1,1,0), new Vertex(2,1,0), new Vertex(3,0,0)};
-		limb = Fabrik.moveLimb(limb, new Vertex(2,-1,0));
+		limb = moveLimb(limb, new Vertex(2,-1,0));
 		for(int i=0;i<limb.length;i++) {
 			System.out.print("(" + limb[i].getX() + "," + limb[i].getY() + "," + limb[i].getZ() + ") ");
 		}
-	}
+	}*/
 	
+	/**
+	 * Constant for the margin of error for acceptable closeness of the end point in the limb to the target.
+	 */
 	public static final double MARGIN_OF_ERROR = 0.01;
 	
+	/**
+	 * Simple FABRIK algorithm. Supports no constraints. Returns an array of vertices ordered from a rigid base to end point.
+	 * 
+	 * @param limb the initial array of vertices
+	 * @param target the location to move the end of the limb to
+	 * @return the array of vertices describing the new location of each joint
+	 */
 	public static Vertex[] moveLimb(Vertex[] limb, Vertex target) {
 		if(limb[limb.length-1].distance(target) < MARGIN_OF_ERROR) return limb;
 		
-		Vertex[] altered = new Vertex[limb.length];
+		Vertex[] forward = new Vertex[limb.length];
 		Vertex currentTarget = new Vertex(target);
 		
 		for(int i=limb.length-1;i>=0;i--) {
-			altered[i] = new Vertex(currentTarget);
-			if(i != 0) {
-				currentTarget.setLocation(currentTarget.getX() - limb[i].distance(limb[i-1]) / currentTarget.distance(limb[i-1])*(currentTarget.getX()-limb[i-1].getX()),
-						currentTarget.getY() - limb[i].distance(limb[i-1]) / currentTarget.distance(limb[i-1])*(currentTarget.getY()-limb[i-1].getY()),
-						currentTarget.getZ() - limb[i].distance(limb[i-1]) / currentTarget.distance(limb[i-1])*(currentTarget.getZ())-limb[i-1].getZ());
-			}
+			forward[i] = new Vertex(currentTarget);
+			if(i != 0)
+				currentTarget = pointAlongVector(currentTarget,limb[i-1],limb[i].distance(limb[i-1]));
 		}
 		
-		Vertex[] altered2 = new Vertex[limb.length];
-		Vertex currentTarget2 = limb[0];
+		Vertex[] backward = new Vertex[limb.length];
+		currentTarget = limb[0];
 		
 		for(int i=0;i<limb.length;i++) {
-			altered2[i] = new Vertex(currentTarget2);
-			if(i != limb.length-1) {
-				currentTarget2.setLocation(currentTarget2.getX() - limb[i].distance(limb[i+1]) / currentTarget2.distance(altered[i+1])*(currentTarget2.getX()-altered[i+1].getX()),
-						currentTarget2.getY() - limb[i].distance(limb[i+1]) / currentTarget2.distance(altered[i+1])*(currentTarget2.getY()-altered[i+1].getY()),
-						currentTarget2.getZ() - limb[i].distance(limb[i+1]) / currentTarget2.distance(altered[i+1])*(currentTarget2.getZ()-altered[i+1].getZ()));
-			}
+			backward[i] = new Vertex(currentTarget);
+			if(i != limb.length-1)
+				currentTarget = pointAlongVector(currentTarget,forward[i+1],limb[i].distance(limb[i+1]));
 		}
-		return moveLimb(altered2,target);
+		return moveLimb(backward,target);
+	}
+	
+	/**
+	 * Returns the point a set distance from the starting point in the direction of the end point.
+	 * 
+	 * @param start the vertex to start with
+	 * @param end the vertex to end the vector
+	 * @param distance the distance along the vector to place the point
+	 * @return the point a set distance on the vector from start to end
+	 */
+	public static Vertex pointAlongVector(Vertex start, Vertex end, double distance) {
+		double[] coordinates = new double[3];
+		for(int i='x';i<='z';i++)
+			coordinates[i-'x'] = start.getN((char)i) - (distance)/(start.distance(end)) * (start.getN((char)i)-end.getN((char)i));
+		return new Vertex(coordinates[0],coordinates[1],coordinates[2]);
 	}
 }
