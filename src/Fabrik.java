fdb554666f3345cbc255f4eecd42e2027ebb8545
Brandon Lewis <brandon.lewis477@comcast.net>
2020-04-12 17:57:32 -0500

    Implemented solveLimb and pointAlongVector for Vertex2D

--- src/Fabrik.java
+++ src/Fabrik.java
@@ -1,17 +1,19 @@
 
 public class Fabrik {
-	public static void main(String args[]) {
-		Vertex3D[] limb = {new Vertex3D(0,0,0), new Vertex3D(1,1,0), new Vertex3D(2,1,0), new Vertex3D(3,0,0)};
-		limb = moveLimb(limb, new Vertex3D(2,-1,0));
+	/*public static void main(String args[]) {
+		Vertex2D[] limb = {new Vertex2D(0,0), new Vertex2D(1,1), new Vertex2D(2,1), new Vertex2D(3,0)};
+		limb = solveLimb(limb, new Vertex2D(2,-1));
 		for(int i=0;i<limb.length;i++) {
-			System.out.print("(" + limb[i].getX() + "," + limb[i].getY() + "," + limb[i].getZ() + ") ");
+			System.out.print("(" + limb[i].getX() + "," + limb[i].getY() + ") ");
 		}
-	}
+	}*/
 	
 	/**
 	 * Constant for the margin of error for acceptable closeness of the end point in the limb to the target.
 	 */
 	public static final double MARGIN_OF_ERROR = 0.01;
+	
+	//3 dimensional algorithms
 	
 	/**
 	 * Simple FABRIK algorithm. Supports no constraints. Returns an array of vertices ordered from a rigid base to end point.
@@ -20,7 +22,7 @@ public class Fabrik {
 	 * @param target the location to move the end of the limb to
 	 * @return the array of vertices describing the new location of each joint
 	 */
-	public static Vertex3D[] moveLimb(Vertex3D[] limb, Vertex3D target) {
+	public static Vertex3D[] solveLimb(Vertex3D[] limb, Vertex3D target) {
 		if(limb[limb.length-1].distance(target) < MARGIN_OF_ERROR) return limb;
 		
 		Vertex3D[] forward = new Vertex3D[limb.length];
@@ -40,7 +42,7 @@ public class Fabrik {
 			if(i != limb.length-1)
 				currentTarget = pointAlongVector(currentTarget,forward[i+1],limb[i].distance(limb[i+1]));
 		}
-		return moveLimb(backward,target);
+		return solveLimb(backward,target);
 	}
 	
 	/**
@@ -56,5 +58,52 @@ public class Fabrik {
 		for(int i='x';i<='z';i++)
 			coordinates[i-'x'] = start.getN((char)i) - (distance)/(start.distance(end)) * (start.getN((char)i)-end.getN((char)i));
 		return new Vertex3D(coordinates[0],coordinates[1],coordinates[2]);
+	}
+	
+	//2 dimensional algorithms
+	
+	/**
+	 * solveLimb modified to work with {@link Vertex2D#}.
+	 * 
+	 * @param limb the initial array of vertices
+	 * @param target the location to move the end of the limb to
+	 * @return the array of vertices describing the new location of each joint
+	 */
+	public static Vertex2D[] solveLimb(Vertex2D[] limb, Vertex2D target) {
+		if(limb[limb.length-1].distance(target) < MARGIN_OF_ERROR) return limb;
+		
+		Vertex2D[] forward = new Vertex2D[limb.length];
+		Vertex2D currentTarget = new Vertex2D(target);
+		
+		for(int i=limb.length-1;i>=0;i--) {
+			forward[i] = new Vertex2D(currentTarget);
+			if(i != 0)
+				currentTarget = pointAlongVector(currentTarget,limb[i-1],limb[i].distance(limb[i-1]));
+		}
+		
+		Vertex2D[] backward = new Vertex2D[limb.length];
+		currentTarget = limb[0];
+		
+		for(int i=0;i<limb.length;i++) {
+			backward[i] = new Vertex2D(currentTarget);
+			if(i != limb.length-1)
+				currentTarget = pointAlongVector(currentTarget,forward[i+1],limb[i].distance(limb[i+1]));
+		}
+		return solveLimb(backward,target);
+	}
+	
+	/**
+	 * pointAlongVector modified to work with {@link=Vertex2D#}.
+	 * 
+	 * @param start the vertex to start with
+	 * @param end the vertex to end the vector
+	 * @param distance the distance along the vector to place the point
+	 * @return the point a set distance on the vector from start to end
+	 */
+	public static Vertex2D pointAlongVector(Vertex2D start, Vertex2D end, double distance) {
+		double[] coordinates = new double[2];
+		for(int i='x';i<='y';i++)
+			coordinates[i-'x'] = start.getN((char)i) - (distance)/(start.distance(end)) * (start.getN((char)i)-end.getN((char)i));
+		return new Vertex2D(coordinates[0],coordinates[1]);
 	}
 }
